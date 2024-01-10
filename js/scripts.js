eb9a8753bff35cd97a55445aa974f3a2d6fc2795
vice2al <vicente@oneohone.io>
2024-01-10 10:01:06 +0000

    Site structure, navbar and some other touches

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 console.log("We're hiring a react frontend developer");
+
+function onNavbarLinkClick(sectionId) {
+  document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth'});
+  return false
+}
